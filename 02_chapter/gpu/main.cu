3c40c67cc2f22b9939a687e002644a7a2a1565b3
Telman Islamov <telman@kakh.ru>
2024-01-03 14:16:04 +0300

    GPU-based vector addition algorithm - the average performace is about 0.7 milliseconds

--- 02_chapter/gpu/main.cu
+++ 02_chapter/gpu/main.cu
@@ -1,5 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <stdint.h>
 #include <math.h>
+
+#define GET_MS(start, stop)	\
+	(stop.tv_sec - start.tv_sec) * 1000.0 + \
+	(stop.tv_usec - start.tv_usec) / 1000.0
 
 static void
 initArray(float* array, int length, float value)
@@ -8,25 +17,81 @@ initArray(float* array, int length, float value)
 		array[i] = value;
 }
 
-static void
+static __global__ void
 add(float* addendum, float* addend, int length)
 {
-	for (int i = 0; i < length; ++i) {
-		addend[i] = addendum[i] + addend[i];
-	}
+	int index = blockIdx.x * blockDim.x + threadIdx.x;
+	
+	if (index < length)
+		addend[index] = addendum[index] + addend[index];
 }
 
 int
 main(int argc, char* argv[])
 {
-	int length = 1 << 20;	// 1 048 576
+	struct timeval start, stop;
+	double elapsed;
+	int length = 1 << 24;	// 16 777 216 ; 29 == 536 870 912
+
+	if (argc > 1)
+		length = 1 << strtol(argv[1], NULL, 10);		
+	
 	float* x = (float*)malloc(length * sizeof(float));
 	float* y = (float*)malloc(length * sizeof(float));
-
+	
 	initArray(x, length, 1.0f);
-	initArray(x, length, 2.0f);
+	initArray(y, length, 2.0f);
 
-	add(x, y, length);
+	float* d_x;
+	float* d_y;
 
+	if (cudaMalloc(&d_x, length * sizeof(float))) {
+		fprintf(stderr, "Failed to allocate memory for d_x\n");
+		free(x);
+		free(y);
+		return (1);
+	}
+
+	if (cudaMalloc(&d_y, length * sizeof(float))) {
+		fprintf(stderr, "Failed to allocate memory for d_y\n");
+		free(x);
+		free(y);
+		cudaFree(d_x);
+		return (1);
+	}
+
+	if (cudaMemcpy(d_x, x, length, cudaMemcpyHostToDevice)) {
+		fprintf(stderr, "Failed to copy from x to d_x\n");
+		free(x);
+		free(y);
+		cudaFree(d_x);
+		cudaFree(d_y);
+		return (1);
+	}
+
+	if (cudaMemcpy(d_y, y, length, cudaMemcpyHostToDevice)) {
+		fprintf(stderr, "Failed to copy from y to d_y\n");
+		free(x);
+		free(y);
+		cudaFree(d_x);
+		cudaFree(d_y);
+		return (1);
+	}
+
+	int blockSize = 256;
+	unsigned int roundedSize = ceil(length / (double)blockSize);
+
+	gettimeofday(&start, NULL);
+	add<<<roundedSize, blockSize>>>(d_x, d_y, length);
+	cudaDeviceSynchronize();
+
+	gettimeofday(&stop, NULL);
+	elapsed = GET_MS(start, stop);
+	printf("elapsed time: %.04f ms.\n", elapsed);
+
+	free(x);
+	free(y);
+	cudaFree(d_x);
+	cudaFree(d_y);
 	return (0);
 }
